cc5eab8373277464d557bb5087845ffd610bb38d
domainKT01 <luisdanielcoronelposada903@gmail.com>
2025-03-02 23:31:17 -0500

    generate graphics template for report v2

--- src/main/java/com/solproe/business/usecase/ReadConfigFileUseCase.java
+++ src/main/java/com/solproe/business/usecase/ReadConfigFileUseCase.java
@@ -7,7 +7,6 @@ import com.solproe.business.repository.ReadConfigFile;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class ReadConfigFileUseCase  {
--- src/main/java/com/solproe/service/config/ReadJsonConfigFile.java
+++ src/main/java/com/solproe/service/config/ReadJsonConfigFile.java
@@ -3,8 +3,6 @@ package com.solproe.service.config;
 import com.solproe.business.repository.ReadConfigFile;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class ReadJsonConfigFile implements ReadConfigFile {
 
